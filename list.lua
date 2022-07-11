e98c76dcfadfbc79ebfb1cfe3712eedc96e4fa07
PaoBlox <109012689+PaoBlox@users.noreply.github.com>
2022-07-11 17:15:08 +0700

    Update list.lua

--- list.lua
+++ list.lua
@@ -1,83 +1,105 @@
 _G.ScriptTable = {
-   ["Blox Fruit"] = {
-      {
-         Show = "String Hub [Mobile & PC]",
-         Link = "https://raw.githubusercontent.com/OPPV01/DEX/main/Protected.lua.txt"
-      },
-      {
-         Show = "Mukuro [PC]",
-         Link = "https://raw.githubusercontent.com/xQuartyx/DonateMe/main/BF"
-      }
-   },
-   ["Demon Fall"] = {
-      {
-         Show = "Lazy Hub",
-         Link = "https://github.com/LioK251/Scripts/raw/main/lazyhub.lua"
-      },
-      {
-         Show = "Ghost Fall [Recommended]",
-         Link = "https://raw.githubusercontent.com/ghost-home/public/main/demonfall.lua"
-      }
-   },
-   ["Goal Kick Simulator"] = {
-      {
-         Show = "Soggyware Hub [PC | Recommend]",
-         Link = "https://raw.githubusercontent.com/SunkenMuch/Soggyware/main/Main"
-      },
-      {
-         Show = "Pufferfish Hub [PC]",
-         Link = "https://raw.githubusercontent.com/Cospog/pufferfish/main/goalKick.lua"
-      },
-      {
-         Show = "Black Gui [PC]",
-         Link = "https://raw.githubusercontent.com/thedragonslayer2/Key-System/main/Load.lua"
-      }
-   },
-   ["Arsenal"] = {
-      {
-         Show = "OwlHub [PC | Recommend]",
-         Link = "https://raw.githubusercontent.com/CriShoux/OwlHub/master/OwlHub.txt"
-      },
-      {
-         Show = "Dark Hub [PC]",
-         Link = "https://darkhub.xyz/remote-script.lua"
-      },
-      {
-         Show = "Dex Hub [PC]",
-         Link = "https://raw.githubusercontent.com/HonestlyDex/DexHub/main/V3.lua"
-      }
-   },
-   ["King Legacy"] = {
-      {
-         Show = "Mukuro [PC | Recommend]",
-         Link = "https://raw.githubusercontent.com/xQuartyx/DonateMe/main/KL"
-      }
-   },
-   ["Your Bizzare Adventure"] = {
-      {
-         Show = "Pink Gui [PC | Recommend]",
-         Link = "https://raw.githubusercontent.com/zakater5/LuaRepo/main/YBA/MainGui.lua"
-      }
-   },
-   ["Test Place A"] = {
-      {
-         Show = "Pink Gui [PC | Recommend]",
-         Link = "https://raw.githubusercontent.com/zakater5/LuaRepo/main/YBA/MainGui.lua"
-      }
-   }
-}
-
-_G.GameTable = {
-   { name = "Blox Fruit", id = 2753915549 },
-   { name = "Blox Fruit", id = 4442272183 },
-   { name = "Blox Fruit", id = 7449423635 },
-   { name = "Demon Fall", id = 5094651510 },
-   { name = "Goal Kick Simulator", id = 9281034297 },
-   { name = "Arsenal", id = 286090429},
-   { name = "King Legacy", id = 4520749081 },
-   { name = "King Legacy", id = 6381829480 },
-   { name = "Your Bizzare Adventure", id = 2809202155 },
-   { name = "Test Place A", id = 7269990871 },
-   --{ name = "Grand Piece Online", id = 5931540094 },
-   --{ name = "King Legacy", id = 5931540094 }
-}
+    ["Blox Fruit"] = {
+       {
+          Show = "String Hub [Mobile & PC]",
+          Link = "https://raw.githubusercontent.com/OPPV01/DEX/main/Protected.lua.txt"
+       },
+       {
+          Show = "Mukuro [PC]",
+          Link = "https://raw.githubusercontent.com/xQuartyx/DonateMe/main/BF"
+       },
+       {
+          Show = "Chiba Hub [Mobile & PC | Recommend ]",
+          Link = "https://raw.githubusercontent.com/KindIhave/ChibaHuB/main/Chiba-BF.txt"
+       }
+    },
+    ["Demon Fall"] = {
+       {
+          Show = "Lazy Hub",
+          Link = "https://github.com/LioK251/Scripts/raw/main/lazyhub.lua"
+       },
+       {
+          Show = "Ghost Fall [ Recommend ]",
+          Link = "https://raw.githubusercontent.com/ghost-home/public/main/demonfall.lua"
+       }
+    },
+    ["Goal Kick Simulator"] = {
+       {
+          Show = "Soggyware Hub [PC | Recommend]",
+          Link = "https://raw.githubusercontent.com/SunkenMuch/Soggyware/main/Main"
+       },
+       {
+          Show = "Pufferfish Hub [PC]",
+          Link = "https://raw.githubusercontent.com/Cospog/pufferfish/main/goalKick.lua"
+       },
+       {
+          Show = "Black Gui [PC]",
+          Link = "https://raw.githubusercontent.com/thedragonslayer2/Key-System/main/Load.lua"
+       }
+    },
+    ["Arsenal"] = {
+       {
+          Show = "OwlHub [PC | Recommend]",
+          Link = "https://raw.githubusercontent.com/CriShoux/OwlHub/master/OwlHub.txt"
+       },
+       {
+          Show = "Dark Hub [PC]",
+          Link = "https://darkhub.xyz/remote-script.lua"
+       },
+       {
+          Show = "Dex Hub [PC]",
+          Link = "https://raw.githubusercontent.com/HonestlyDex/DexHub/main/V3.lua"
+       }
+    },
+    ["King Legacy"] = {
+       {
+          Show = "Mukuro [PC | Recommend]",
+          Link = "https://raw.githubusercontent.com/xQuartyx/DonateMe/main/KL"
+       }
+    },
+    ["Your Bizzare Adventure"] = {
+       {
+          Show = "Bitch Boy [PC | Recommend]",
+          Link = "https://raw.githubusercontent.com/zakater5/LuaRepo/main/YBA/MainGui.lua"
+       },
+       {
+        Show = "YBA GUI REWRITE [PC | Recommend]",
+        Link = "https://raw.githubusercontent.com/Tobias020108Back/YBA-AUT/main/YBA-GUI-Rewrite.lua"
+        }
+    },
+    ["Grand Piece Online"] = {
+        {
+           Show = "Yellow Gui [PC | Easy to Use ]",
+           Link = "https://raw.githubusercontent.com/Jakekill871/public-scripts/master/GrandPieceOnlineGUI"
+        },
+        {
+         Show = "CFA Hub [PC | Recommend]",
+         Link = "https://raw.githubusercontent.com/CFA-HUB/CFA-HUB/main/cfahubfree.lua"
+        },
+        {
+         Show = "Trigon [PC | Fruit Notifier | Key System ]",
+         Link = "https://raw.githubusercontent.com/ArponAG/Scripts/main/gpo.lua"
+        }
+     },
+     ["Anime Dimension Simulator"] = {
+        {
+           Show = "Sun Hub [PC | Recommend ]",
+           Link = "https://raw.githubusercontent.com/Besty191/MAZI/main/Anime%20Dimensions%20Simulator%20%7C%20Sun%20Hub"
+        }
+     }
+ }
+ 
+ _G.GameTable = {
+    { name = "Blox Fruit", id = 2753915549 },
+    { name = "Blox Fruit", id = 4442272183 },
+    { name = "Blox Fruit", id = 7449423635 },
+    { name = "Demon Fall", id = 5094651510 },
+    { name = "Goal Kick Simulator", id = 9281034297 },
+    { name = "Arsenal", id = 286090429},
+    { name = "King Legacy", id = 4520749081 },
+    { name = "King Legacy", id = 6381829480 },
+    { name = "Your Bizzare Adventure", id = 2809202155 },
+    { name = "Grand Piece Online", id = 1 },
+    { name = "Anime Dimension Simulator", id = 1 },
+    --{ name = "King Legacy", id = 5931540094 }
+ }
