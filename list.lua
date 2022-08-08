1b184722af70d5bcfb0916a363408edf4fe13861
PaoBlox <109012689+PaoBlox@users.noreply.github.com>
2022-08-08 20:44:14 +0700

    Update list.lua

--- list.lua
+++ list.lua
@@ -1,23 +1,23 @@
 _G.ScriptTable = {
     ["Blox Fruit"] = {
        {
-          Show = "String Hub [Mobile & PC]",
+          Show = "String Hub [ Mobile & PC ]",
           Link = "https://raw.githubusercontent.com/OPPV01/DEX/main/Protected.lua.txt"
        },
        {
-          Show = "Mukuro [PC]",
+          Show = "Mukuro [ PC ]",
           Link = "https://raw.githubusercontent.com/xQuartyx/DonateMe/main/NEWBF"
        },
        {
-          Show = "Chiba Hub [Mobile & PC | Recommend ]",
+          Show = "Chiba Hub [ Mobile & PC | Recommend ]",
           Link = "https://raw.githubusercontent.com/KindIhave/ChibaHuB/main/Chiba-BF.txt"
        },
        {
-          Show = "Ripper Hub [PC | Recommend ]",
+          Show = "Ripper Hub [ PC | Recommend ]",
           Link = "https://raw.githubusercontent.com/hajibeza/RIPPER-HUB/main/Kaitun.txt"
        },
        {
-          Show = "Chiba Hub [Mobile & PC | Recommend ]",
+          Show = "Chiba Hub [ Mobile & PC | Recommend ]",
           Link = "https://raw.githubusercontent.com/KindIhave/ChibaHuB/main/Chiba-BF.txt"
        }
     },
@@ -29,93 +29,93 @@ _G.ScriptTable = {
     },
     ["Goal Kick Simulator"] = {
        {
-          Show = "Soggyware Hub [PC | Recommend]",
+          Show = "Soggyware Hub [ PC | Recommend]",
           Link = "https://raw.githubusercontent.com/SunkenMuch/Soggyware/main/Main"
        },
        {
-          Show = "Pufferfish Hub [PC]",
+          Show = "Pufferfish Hub [ PC ]",
           Link = "https://raw.githubusercontent.com/Cospog/pufferfish/main/goalKick.lua"
        },
        {
-          Show = "Black Gui [PC]",
+          Show = "Black Gui [ PC ]",
           Link = "https://raw.githubusercontent.com/thedragonslayer2/Key-System/main/Load.lua"
        }
     },
     ["Arsenal"] = {
        {
-          Show = "OwlHub [PC | Recommend]",
+          Show = "OwlHub [ PC | Recommend]",
           Link = "https://raw.githubusercontent.com/CriShoux/OwlHub/master/OwlHub.txt"
        },
        {
-          Show = "Dark Hub [PC]",
+          Show = "Dark Hub [ PC]",
           Link = "https://darkhub.xyz/remote-script.lua"
        },
        {
-          Show = "Dex Hub [PC]",
+          Show = "Dex Hub [ PC]",
           Link = "https://raw.githubusercontent.com/HonestlyDex/DexHub/main/V3.lua"
        }
     },
     ["King Legacy"] = {
        {
-          Show = "Mukuro [PC | Recommend]",
+          Show = "Mukuro [ PC | Recommend]",
           Link = "https://raw.githubusercontent.com/xQuartyx/DonateMe/main/KL"
        }
     },
     ["Your Bizzare Adventure"] = {
        {
-          Show = "Bitch Boy [PC | Recommend]",
+          Show = "Bitch Boy [ PC | Recommend]",
           Link = "https://raw.githubusercontent.com/zakater5/LuaRepo/main/YBA/MainGui.lua"
        },
        {
-        Show = "YBA GUI REWRITE [PC | Recommend]",
+        Show = "YBA GUI REWRITE [ PC | Recommend]",
         Link = "https://raw.githubusercontent.com/Tobias020108Back/YBA-AUT/main/YBA-GUI-Rewrite.lua"
         }
     },
     ["Grand Piece Online"] = {
         {
-           Show = "Yellow Gui [PC | Easy to Use ]",
+           Show = "Yellow Gui [ PC | Easy to Use ]",
            Link = "https://raw.githubusercontent.com/Jakekill871/public-scripts/master/GrandPieceOnlineGUI"
         },
         {
-         Show = "CFA Hub [PC | Recommend]",
+         Show = "CFA Hub [ PC | Recommend]",
          Link = "https://raw.githubusercontent.com/CFA-HUB/CFA-HUB/main/cfahubfree.lua"
         },
         {
-         Show = "Trigon [PC | Fruit Notifier | Key System ]",
+         Show = "Trigon [ PC | Fruit Notifier | Key System ]",
          Link = "https://raw.githubusercontent.com/ArponAG/Scripts/main/gpo.lua"
         },
         {
-         Show = "Auto OSU / Ken Haki v2 [PC | Recommend ]",
+         Show = "Auto OSU / Ken Haki v2 [ PC | Recommend ]",
          Link = "https://raw.githubusercontent.com/PaoBlox/paohub/main/paoautoosu.lua"
         }
      },
      ["Anime Dimension Simulator"] = {
         {
-           Show = "Sun Hub [PC | Recommend ]",
+           Show = "Sun Hub [ PC | Recommend ]",
            Link = "https://raw.githubusercontent.com/Besty191/MAZI/main/Anime%20Dimensions%20Simulator%20%7C%20Sun%20Hub"
         }
      },
      ["Mining Simulator 2"] = {
         {
-           Show = "Vestra Hub [PC | Recommend ]",
+           Show = "Vestra Hub [ PC | Recommend ]",
            Link = "https://script.vestrahub.com/"
         }
      },
      ["Combat Warrior"] = {
         {
-           Show = "Project Hook [PC | Recommend ]",
+           Show = "Project Hook [ PC | Recommend ]",
            Link = "https://projecthook.xyz/scripts/new-free.lua"
         },
         {
-           Show = "Yellow Gui [PC | Recommend ]",
+           Show = "Yellow Gui [ PC | Recommend ]",
            Link = "https://raw.githubusercontent.com/IsaaaKK/cwhb/main/cw.txt"
         },
         {
-           Show = "UwUware [PC | Recommend ]",
+           Show = "UwUware [ PC | Recommend ]",
            Link = "https://raw.githubusercontent.com/Jan5106/uwuware_final/main/main.lua"
         },
         {
-           Show = "Bird Hub [PC | Recommend ]",
+           Show = "Bird Hub [ PC | Recommend ]",
            Link = "https://raw.githubusercontent.com/scriptpastebin/raw/main/BirdCombatWarriors"
         },
      },
@@ -125,16 +125,32 @@ _G.ScriptTable = {
            Link = "https://raw.githubusercontent.com/1f0yt/community/master/new"
         },
         {
-           Show = "Keybrew Hub [PC | Recommend ]",
+           Show = "Keybrew Hub [ PC | Recommend ]",
            Link = "https://raw.githubusercontent.com/borntodiekuv/KeybrewHub/main/Main"
         },
         {
-           Show = "LeadMarker [PC | Recommend ]",
+           Show = "LeadMarker [ PC | Recommend ]",
            Link = "https://raw.githubusercontent.com/LeadMarker/Scripts/main/ProjectSlayer.lua"
         },
         {
-           Show = "Auto Spin Clan / Family [PC & Mobile | Recommend ]",
+           Show = "Auto Spin Clan / Family [ PC & Mobile | Recommend ]",
            Link = "https://raw.githubusercontent.com/PaoBlox/paohub/main/paoautospinps.lua"
+        }
+     },
+     ["Sea Piece"] = {
+        {
+           Show = "BadHub [ PC | Recommend ]",
+           Link = "https://raw.githubusercontent.com/idkfffff/SeaPiece/main/BadHub.lua"
+        },
+        {
+           Show = "Lazy Hub [ PC ]",
+           Link = "https://raw.githubusercontent.com/idkfffff/SeaPiece/main/BadHub.lua"
+        }
+     },
+     ["Era Of Althea"] = {
+        {
+           Show = "Black GUI [ PC | Recommend ]",
+           Link = "https://raw.githubusercontent.com/XTheMasterX/Scripts/Main/EraOfAlthea"
         }
      }
  }
@@ -154,5 +170,7 @@ _G.ScriptTable = {
     { name = "Mining Simulator 2", id = 9551640993 },
     { name = "Combat Warrior", id = 1 },
     { name = "Project Slayers", id = 5956785391 },
-    { name = "Project Slayers", id = 6152116144 }
+    { name = "Project Slayers", id = 6152116144 },
+    { name = "Sea Piece", id = 9744597824 },
+    { name = "Era Of Althea", id = 9840387028 }
  }
