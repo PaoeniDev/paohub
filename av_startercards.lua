4ab8a37179231cf43bca28d14f334c00a5001577
Pao <109012689+PaoeniDev@users.noreply.github.com>
2025-03-28 08:01:02 +0700

    Update av_startercards.lua

--- av_startercards.lua
+++ av_startercards.lua
@@ -1,4 +1,6 @@
-repeat task.wait() until game:IsLoaded()
+repeat
+    task.wait()
+until game:IsLoaded()
 
 local player = game:GetService("Players").LocalPlayer
 local playerGUI = player:WaitForChild("PlayerGui")
@@ -6,16 +8,39 @@ local playerGUI = player:WaitForChild("PlayerGui")
 local CardsFrame = playerGUI.Frames.Modifiers
 local id = math.random(0, 9999)
 
-getgenv().pao_runningID = id
+getfenv().pao_runningID = id
+
+function choose(modifier)
+    local args = {
+        [1] = "Choose",
+        [2] = modifier
+    }
+
+    game:GetService("ReplicatedStorage").Networking.ModifierEvent:FireServer(unpack(args))
+end
 
 CardsFrame.VoteTypeLabel:GetPropertyChangedSignal("Visible"):Connect(function()
-  if getgenv().pao_runningID == id and CardsFrame.VoteTypeLabel.Visible == true then
-    for i, v in pairs(CardsFrame.Main:GetChildren()) do
-      if v:IsA("Frame") then
-        print(v.Name)
+    if getfenv().pao_runningID == id and CardsFrame.VoteTypeLabel.Visible == true then
+
+      local done = false
+
+      for _, p in pairs(getfenv().priority) do
+        if done then break end
+        for __, v in pairs(CardsFrame.Main:GetChildren()) do
+          if v:IsA("Frame") and p == v.Name then
+
+            done = true
+            choose(p)
+
+            break
+          end
+        end
+      end
+
+      if not done then
+        print("NOT FOUND")
       end
     end
-  end
 end)
 
 print("pao.")
