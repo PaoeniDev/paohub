256e3d84cff4550d9e04c65c676737e41008db6f
Pao <109012689+PaoeniDev@users.noreply.github.com>
2025-03-28 07:02:09 +0700

    Create av_startercards.lua

---
+++ av_startercards.lua
@@ -0,0 +1,16 @@
+repeat task.wait() until game:IsLoaded()
+
+local player = game:GetService("Players").LocalPlayer
+local playerGUI = player:WaitForChild("PlayerGui")
+
+local CardsFrame = playerGUI.Frames.Modifiers
+
+CardsFrame.VoteTypeLabel:GetPropertyChangedSignal("Visible"):Connect(function()
+  if CardsFrame.VoteTypeLabel.Visible == true then
+    for i, v in pairs(CardsFrame.Main) do
+      if v:IsA("Frame") then
+        print(v.Name)
+      end
+    end
+  end
+end)
