d3bd9f0103cb521e663e1168ca09c8c342e40193
Pao <109012689+PaoeniDev@users.noreply.github.com>
2025-03-28 07:09:09 +0700

    Update av_startercards.lua

--- av_startercards.lua
+++ av_startercards.lua
@@ -4,9 +4,12 @@ local player = game:GetService("Players").LocalPlayer
 local playerGUI = player:WaitForChild("PlayerGui")
 
 local CardsFrame = playerGUI.Frames.Modifiers
+local id = math.random(0, 9999)
+
+getgenv().pao_runningID = id
 
 CardsFrame.VoteTypeLabel:GetPropertyChangedSignal("Visible"):Connect(function()
-  if CardsFrame.VoteTypeLabel.Visible == true then
+  if getgenv().pao_runningID == id and CardsFrame.VoteTypeLabel.Visible == true then
     for i, v in pairs(CardsFrame.Main:GetChildren()) do
       if v:IsA("Frame") then
         print(v.Name)
